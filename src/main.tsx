e06249aad729a56799da93818cfc1067e2b7e181
Evgeny Videneev <evidenee@akamai.com>
2026-04-27 21:03:36 +0200

    feat(app): wire providers and Shell into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { Shell } from './app/layout/Shell';
+import { RepositoryProvider } from './app/providers/RepositoryProvider';
+import { DataProvider } from './app/providers/DataProvider';
+import { UserProfileProvider } from './app/providers/UserProfileProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RepositoryProvider>
+      <DataProvider>
+        <UserProfileProvider>
+          <Shell>
+            <App />
+          </Shell>
+        </UserProfileProvider>
+      </DataProvider>
+    </RepositoryProvider>
   </StrictMode>,
 );
